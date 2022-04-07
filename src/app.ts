57470114bd3cc00f19d80b368caa666f9c9a6d0e
rahmatsyifana12 <adean435@gmail.com>
2022-04-07 20:50:43 +0700

    Remove unnecessary code

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import globalRouter from './routes';
 
 dotenv.config();
 
@@ -10,8 +9,5 @@ const app = express();
 // global middlewares
 app.use(express.json());
 app.use(cors());
-
-// bind global router to all routes
-// app.use('/', globalRouter);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -6,15 +6,6 @@ import connectionConfig from './ormconfig';
 
 const port = process.env.PORT || 5000;
 
-// createConnection()
-//     .then(() => {
-//         const port = process.env.PORT || 5000;
-
-//         app.listen(port, () =>
-//             console.log(`Server is hosted at http://localhost:${port}/`));
-//     })
-//     .catch((err) => console.error(err));
-
 app.listen(port, async () => {
     app.use('/', globalRouter);
     await createConnection(connectionConfig);
